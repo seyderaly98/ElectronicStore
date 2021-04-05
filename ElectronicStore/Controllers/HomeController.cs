6362acbe019aec6fde80f073a5fa5fe50ad26ce8
Almat <seyderaly98@gmail.com>
2021-04-05 08:33:51 +0600

    Добавлен асеты

--- ElectronicStore/Controllers/HomeController.cs
+++ ElectronicStore/Controllers/HomeController.cs
@@ -6,16 +6,19 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ElectronicStore.Models;
+using ElectronicStore.Models.Data;
 
 namespace ElectronicStore.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private ElectronicStoreContext _db { get; set; }
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ElectronicStoreContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
