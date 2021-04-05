b4898e528b2d6084c42e4b0bba6cc37301efb00d
Almat <seyderaly98@gmail.com>
2021-04-05 09:50:52 +0600

    Сверстан страница сайта о нас

--- ElectronicStore/Controllers/HomeController.cs
+++ ElectronicStore/Controllers/HomeController.cs
@@ -30,6 +30,10 @@ namespace ElectronicStore.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
